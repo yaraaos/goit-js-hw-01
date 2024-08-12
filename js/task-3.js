6ee6e0e2c2dfcaf82590aec87c3aa9dc9db004e4
yaraaos <yara144ss@gmail.com>
2024-08-13 00:50:01 +0300

    Task 1,2,3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+/* Ширина елемента */
+
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingtWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border) * 2; 
+    const totalWidth = contentWidth + paddingtWidth + borderWidth;
+    return totalWidth;
+}
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
